d38c49b38cd0d9e4f84353c41eb9c9327ef6920f
Tanya Parmar <tanya.parmar@thoughtworks.com>
2023-12-10 22:48:17 +0100

    create DB with admin and developer user

--- application/create_db.py
+++ application/create_db.py
@@ -1,2 +1,11 @@
-from app import db
+# create_db.py
+from app import db, User
 db.create_all()
+
+# Create users
+admin_user = User(username='admin', password='admin_123', is_admin=True)
+developer_user = User(username='developer', password='developer_456', is_admin=False)
+
+db.session.add(admin_user)
+db.session.add(developer_user)
+db.session.commit()
